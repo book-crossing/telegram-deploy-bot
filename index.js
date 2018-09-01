5b4ec27e6e52e72fccbc74f6b537328bd27450f5
Karen <dev.kaskar2008@gmail.com>
2018-09-02 02:16:36 +0300

    remove sendMessage

--- index.js
+++ index.js
@@ -29,8 +29,6 @@ const urlMap = {
 http.createServer(function (req, res) {
   console.log(req);
 
-  slimbot.sendMessage(chatId, JSON.stringify(req));
-
   let handler = urlMap[req.url];
 
   if (typeof handler === 'function') {
