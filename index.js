8c4c3215b24cad080f3c58632bed465b064ae61d
Karen <dev.kaskar2008@gmail.com>
2018-09-02 01:52:47 +0300

    Initial-D

---
+++ index.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+const http = require('http');
+
+const chatId = process.env['TELEGRAM_CHAT_ID'];
+
+const Slimbot = require('slimbot');
+const slimbot = new Slimbot(process.env['TELEGRAM_BOT_TOKEN'] || '');
+
+// Register listeners
+
+// slimbot.on('message', message => {
+//   slimbot.sendMessage(message.chat.id, 'Message received');
+// });
+
+// Call API
+
+slimbot.startPolling();
+
+const urlMap = {
+  '/building': () => {
+    slimbot.sendMessage(chatId, 'Building...');
+  },
+  '/deployed': () => {
+    slimbot.sendMessage(chatId, 'Deployed...');
+  }
+};
+
+http.createServer(function (req, res) {
+  console.log(req);
+
+  let handler = urlMap[req.url];
+
+  if (typeof handler === 'function') {
+    handler();
+  }
+  res.end();
+}).listen(process.env['PORT'] || 3030);
