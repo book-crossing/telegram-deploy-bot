3f63441b909ea77a6f49b19c0b1ed86f31d26949
Karen <dev.kaskar2008@gmail.com>
2018-09-02 12:40:05 +0300

    improved hook handler

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const http = require('http');
+const { parse } = require('querystring');
 
 const chatId = process.env['TELEGRAM_CHAT_ID'];
 
@@ -9,20 +10,43 @@ const slimbot = new Slimbot(process.env['TELEGRAM_BOT_TOKEN'] || '');
 
 slimbot.startPolling();
 
-const urlMap = {
-  '/building': () => {
-    slimbot.sendMessage(chatId, '🚧 Building book-crossing backend dev server...');
+const resourceHandler = {
+  'build': (body) => {
+    let appName = body.data.app.name || 'unknown app';
+    slimbot.sendMessage(chatId, '🚧 Building ${appName} backend dev server...');
   },
-  '/deployed': () => {
-    slimbot.sendMessage(chatId, '✔️ Deployed book-crossing backend dev server!');
+  'release': (body) => {
+    let appName = body.data.app.name || 'unknown app';
+    slimbot.sendMessage(chatId, '✔️ Deployed ${appName} backend dev server!');
   }
 };
 
-http.createServer(function (req, res) {
-  let handler = urlMap[req.url];
-
-  if (typeof handler === 'function') {
-    handler();
+function parseBody(req) {
+  if (req.method === 'POST') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      req.body = parse(body);
+    });
   }
-  res.end('ok');
+  return req;
+}
+
+http.createServer(function (req, res) {
+  if (req.method === 'POST') {
+    req = parseBody(req);
+
+    let resource = req.body.resource;
+    let handler = resourceHandler[resource];
+
+    if (typeof handler === 'function') {
+      handler(req.body);
+      res.end('ok');
+    }
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
 }).listen(process.env['PORT'] || 3030, process.env['HOST'] || 'localhost');
